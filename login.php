6ec03f4e9195e6c5e8a69ee777ea4c4b31863cd6
Lucas Augusto de Almeida <luuaugusto12345@gmail.com>
2020-07-18 12:40:25 -0300

    alteração para senha em md5

--- login.php
+++ login.php
@@ -11,7 +11,7 @@ if(empty($_POST['usuario']) || empty($_POST['senha'])){
 $usuario = mysqli_real_escape_string($conexao, $_POST['usuario']);
 $senha = mysqli_real_escape_string($conexao, $_POST['senha']);
 
-$query = "select idusuario, usuario from usuario where usuario = '{$usuario}' and senha = '{$senha}'"; 
+$query = "select idusuario, usuario from usuario where usuario = '{$usuario}' and senha = md5('{$senha}')"; 
 
 $result = mysqli_query($conexao, $query);
 $row = mysqli_num_rows($result);
